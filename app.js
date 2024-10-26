337a6956c7569bb79ef95d98dade49085160e767
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-26 11:39:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// hamburger
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -7,6 +9,14 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+
+// hamburger
+
+
+
+
+
+
 
 const coords = { x: 0, y: 0 };
 const circles = document.querySelectorAll(".circle");
@@ -49,3 +59,45 @@ function animationCircles() {
     requestAnimationFrame(animationCircles);
 }
 animationCircles();
+
+
+
+
+
+
+
+
+
+
+
+
+(function () {
+
+    const link = document.querySelectorAll('nav > .link');
+    const cursor = document.querySelector('.cursor');
+
+    const animateit = function (e) {
+          const span = this.querySelector('.link > span');
+          const { offsetX: x, offsetY: y } = e,
+          { offsetWidth: width, offsetHeight: height } = this,
+
+          move = 25,
+          xMove = x / width * (move * 2) - move,
+          yMove = y / height * (move * 2) - move;
+
+          span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+          if (e.type === 'mouseleave') span.style.transform = '';
+    };
+
+    const editCursor = e => {
+          const { clientX: x, clientY: y } = e;
+          cursor.style.left = x + 'px';
+          cursor.style.top = y + 'px';
+    };
+
+    link.forEach(b => b.addEventListener('mousemove', animateit));
+    link.forEach(b => b.addEventListener('mouseleave', animateit));
+    window.addEventListener('mousemove', editCursor);
+
+})();
